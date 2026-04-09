fdeaa2bb1453d3a59982a692c9c354b25aa03fa8
joschonarth <joschonarth@gmail.com>
2026-04-09 18:59:54 -0300

    feat: implement Customer section

---
+++ src/components/customer-story-section/customer-story-section.tsx
@@ -0,0 +1,75 @@
+import { PT_Sans_Caption } from 'next/font/google'
+import Image from 'next/image'
+
+const ptSansCaption = PT_Sans_Caption({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+})
+
+const customerStories = [
+  {
+    content:
+      'Criar minha loja com o site.set foi a melhor decisão para o meu negócio. A plataforma é super intuitiva, e consegui colocar meus produtos à venda em poucos minutos.',
+    author: {
+      name: 'Annete Bones',
+      role: 'CEO na Anne Corp',
+      avatar: '/customer-01.png',
+    },
+  },
+  {
+    content:
+      'Transformar minha ideia em uma loja online foi fácil e rápido. Adorei as opções de personalização e a simplicidade para gerenciar os pedidos. Já vejo meus produtos alcançando mais pessoas!',
+    author: {
+      name: 'Jacob Jones',
+      role: 'CEO na JJ Corp',
+      avatar: '/customer-02.png',
+    },
+  },
+]
+
+export const CustomerStorySection = () => {
+  return (
+    <section className="py-8 md:py-10">
+      <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-12 px-4 sm:px-6 lg:px-8">
+        <h2
+          className={`${ptSansCaption.className} text-gray-100 text-heading-xl`}
+        >
+          Quem utiliza, aprova!
+        </h2>
+
+        <div className="grid gap-8 md:grid-cols-2">
+          {customerStories.map((customerStory) => (
+            <div
+              className="flex flex-col gap-6 rounded-lg bg-gray-500 p-6 md:p-12"
+              key={customerStory.author.name}
+            >
+              <p className="text-balance text-gray-200">
+                {customerStory.content}
+              </p>
+
+              <div className="flex items-center gap-3">
+                <div className="relative h-10 w-10 overflow-hidden rounded-full">
+                  <Image
+                    alt={customerStory.author.name}
+                    className="object-cover"
+                    fill
+                    src={customerStory.author.avatar}
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <strong className="text-gray-200 text-sm">
+                    {customerStory.author.name}
+                  </strong>
+                  <span className="text-gray-300 text-xs">
+                    {customerStory.author.role}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
