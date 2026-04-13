73064161a733715a1d73f14ca9e5d5f0f9b32960
joschonarth <joschonarth@gmail.com>
2026-04-13 19:04:49 -0300

    feat: create first post with Contentlayer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+import { allPosts } from 'contentlayer/generated'
 import { LandingPage } from '@/templates/landing-page'
 
 export default function Home() {
+  console.log(allPosts)
+
   return <LandingPage />
 }
