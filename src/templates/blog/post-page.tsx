d51913c1ad0e3c565bee960371a8096b77b79caa
joschonarth <joschonarth@gmail.com>
2026-04-21 22:01:24 -0300

    refactor: turn post page to a template blog

---
+++ src/templates/blog/post-page.tsx
@@ -0,0 +1,120 @@
+import { allPosts } from 'contentlayer/generated'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { Avatar } from '@/components/avatar'
+import { Markdown } from '@/components/markdown'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+import { Button } from '@/components/ui/button'
+import { useShare } from '@/hooks'
+
+export const PostPage = () => {
+  const router = useRouter()
+  const slug = router.query.slug as string
+
+  const post = allPosts.find(
+    (post) => slug && post.slug.toLowerCase() === slug.toLowerCase()
+  )
+
+  const postUrl = `https://site.set/blog/${slug}`
+
+  const { shareButtons } = useShare({
+    url: postUrl,
+    title: post?.title,
+    text: post?.description,
+  })
+
+  if (!post) {
+    return null
+  }
+
+  const publishedDate = new Date(post?.date).toLocaleDateString('pt-BR')
+
+  return (
+    <main className="mt-32 text-gray-100">
+      <div className="container space-y-12 px-4 md:px-8">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild className="text-action-sm">
+                <Link href="/blog">Blog</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <span className="text-action-sm text-blue-200">
+                {post?.title}
+              </span>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-[1fr_300px] lg:gap-12">
+          <article className="overflow-hidden rounded-lg border-[1px] border-gray-400 bg-gray-600">
+            <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
+              <Image
+                alt={post?.title ?? ''}
+                className="object-cover"
+                fill
+                src={post?.image ?? ''}
+              />
+            </figure>
+
+            <header className="mt-8 p-4 pb-0 md:mt-12 md:p-6 lg:p-12">
+              <h1 className="mb-8 text-balance text-heading-lg md:text-heading-xl lg:text-heading-xl">
+                {post?.title}
+              </h1>
+
+              <Avatar.Container>
+                <Avatar.Image
+                  alt={post?.title}
+                  size="sm"
+                  src={post.author.avatar.trim()}
+                />
+                <Avatar.Content>
+                  <Avatar.Title>{post?.author.name}</Avatar.Title>
+                  <Avatar.Description>
+                    Publicado em {''}
+                    <time dateTime={post.date}>{publishedDate}</time>
+                  </Avatar.Description>
+                </Avatar.Content>
+              </Avatar.Container>
+            </header>
+
+            <div className="prose prose-invert mt-12 max-w-none px-4 md:px-6 lg:px-12">
+              <Markdown content={post.body.raw} />
+            </div>
+          </article>
+
+          <aside className="space-y-6">
+            <div className="rounded-lg bg-gray-700">
+              <h2 className="mb-4 text-gray-100 text-heading-xs">
+                Compartilhar
+              </h2>
+
+              <div className="space-y-3">
+                {shareButtons.map((provider) => (
+                  <Button
+                    className="w-full justify-start gap-2"
+                    key={provider.provider}
+                    onClick={() => provider.action()}
+                    variant="outline"
+                  >
+                    {provider.icon}
+                    {provider.name}
+                  </Button>
+                ))}
+              </div>
+            </div>
+          </aside>
+        </div>
+      </div>
+    </main>
+  )
+}
